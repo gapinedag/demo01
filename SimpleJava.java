3728aa8399b1bfff739495e9a1dc09a184fd56a9
gapinedag <gapinedag@gmail.com>
2022-04-30 15:38:51 +0000

    Third commit (POll SCM #2)

--- SimpleJava.java
+++ SimpleJava.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-        System.out.println("Hello world!\nPoll SCM Test...");
+        System.out.println("Hello world!\nPoll SCM Test #2...");
     }
 }
